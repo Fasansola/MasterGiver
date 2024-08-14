569fa73e9796dd43a0a1f2a05c858cbbc753937c
Fasan Olusola <justwpstudio@gmail.com>
2024-08-14 13:07:59 +0100

    Updated settings for production

--- MasterGiver/settings.py
+++ MasterGiver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
